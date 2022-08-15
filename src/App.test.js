1953bdebf119604b1637b309d3c366f9da309c27
두현컴 <doodream17@yunikeom-ui-MacBookAir.local>
2022-08-16 00:15:49 +0900

    feature : getByRole 접근방식 사용해보기

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("리액트 배우기");
+  const linkElement = screen.getByRole('link', { name : "리액트 배우기" });
   expect(linkElement).toBeInTheDocument();
 });
