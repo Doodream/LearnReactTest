737b16c7b17871587be6d95f49de0d94d05328b8
Luke <doodream17@yunikeom-ui-MacBookAir.local>
2022-08-27 09:25:39 +0900

    fix : 테스트 케이스의 난수 생성자는 연산에 대한 테스트로서 의미가 없습니다.

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,22 +1,22 @@
 import { add, multiplication, minus ,divide } from './calculator'
 
 describe('사칙 연산 테스트', ()=>{
-  const randomNumber1 = Math.round(Math.random() * 10)
-  const randomNumber2 = Math.round(Math.random() * 10)
+  const testNumber1 = 4
+  const testNumber2 = 23
   test('덧셈', ()=>{
-    expect(add(randomNumber1, randomNumber2)).toBe(randomNumber1 + randomNumber2)
+    expect(add(testNumber1, testNumber2)).toBe(testNumber1 + testNumber2)
   })
 
   test('뺄셈', () => {
-    expect(minus(randomNumber1, randomNumber2)).toBe(randomNumber1 - randomNumber2)
+    expect(minus(testNumber1, testNumber2)).toBe(testNumber1 - testNumber2)
   })
 
   test('나눗셈', () => {
-    expect(divide(randomNumber1, randomNumber2)).toBe(randomNumber1 / randomNumber2)
+    expect(divide(testNumber1, testNumber2)).toBe(testNumber1 / testNumber2)
   })
 
   test('곱셈', () => {
-    expect(multiplication(randomNumber1, randomNumber2)).toBe(randomNumber1 * randomNumber2)
+    expect(multiplication(testNumber1, testNumber2)).toBe(testNumber1 * testNumber2)
   })
 
 })
